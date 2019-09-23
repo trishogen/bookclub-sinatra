f6e98cd52080d1eabb8bd006c1010e4975be81f7
trishogen <patriciaogen@gmail.com>
2019-09-22 22:29:21 -0400

    allos user to login (bug in bcrypt)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,42 @@ class UsersController < ApplicationController
     if params[:username] == "" or params[:password] == "" or params[:email] == ""
       redirect '/signup'
     else
-      user = User.create(:username => params[:username], :password_digest => params[:password], :email => params[:email])
+      user = User.new(:username => params[:username], :password_digest => params[:password], :email => params[:email])
+      user.save
       session[:user_id] = user.id
       redirect '/' # TODO: redirect to their bookclub page
     end
+  end
+
+  get '/login' do
+    if !!logged_in?
+      redirect '/' # TODO: redirect to their bookclub page
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/' # TODO: redirect to their bookclub page
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/logout' do
+    if !!logged_in?
+      erb :'users/logout'
+    else
+      redirect '/'
+    end
+  end
+
+  post '/logout' do
+    session.destroy
+    redirect '/'
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :clubs, through: :user_clubs
+
+  has_secure_password
 end
