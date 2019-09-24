0751d26f918497bd83a404ba70b9bff86c451c28
trishogen <patriciaogen@gmail.com>
2019-09-24 18:47:56 -0400

    clubs are slugifiable

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,13 @@
 class Club < ActiveRecord::Base
   has_many :posts
   has_many :users, through: :user_clubs
+
+  def slug
+    self.name.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|club| club.slug == slug}
+  end
+
 end
